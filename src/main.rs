85298267b9b3f50877ec364409df5181de53e167
lesnikovskiy <lesnikovski@gmail.com>
2024-08-01 00:16:45 +0300

    Add bank entities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+struct Account {
+    balance: u32,
+    id: i32,
+    holder: String,
+}
+
+#[derive(Debug)]
+struct Bank {
+    name: String,
+    accounts: Vec<Account>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
